bee81efbaaf42964cfc8226de45a9f7c6fde85f7
Asrani-Aman <amanasrani1712@gmail.com>
2024-03-07 00:19:23 +0530

    nambar hamburger icon coreection , hero section responsiveness

--- sections/Hero.js
+++ sections/Hero.js
@@ -25,7 +25,7 @@ const Hero = () => {
       <section className="hero-sec">
         <div className="container">
           <div className="heading-title">
-            <Title title="The last digital agency you ll ever need" />
+            <Title title="The last digital agency you'll ever need" />
             <p>
               Suspendisse ut magna porttitor, sollicitudin ligula at, molestie
               dolor. Vivamus a ligula ut velit placerat egestas at id leo. Nulla
